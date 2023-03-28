401b5b405b2c8a5c067c0b8c72570930e9b18727
Wilder <lilianbataille6263@gmail.com>
2023-03-28 11:06:59 +0200

    Reussi

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,9 @@
+function Pokedex(){
+    return <figure>
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+             alt="Pokemon de type plante, bulbasaur" /> 
+        <figcaption>bulbasaur</figcaption>
+    </figure>
+}
+
+export default Pokedex;
