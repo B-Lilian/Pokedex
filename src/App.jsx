e723a765072c10e68ebdcdc91f3b37a341bb2fd3
Wilder <lilianbataille6263@gmail.com>
2023-03-28 17:17:53 +0200

    Affichage dynamique

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Pokedex from "./components/PokemonCard.jsx";
+import PokemonCard from "./components/PokemonCard.jsx";
 
 function App() {
   return <div className="App">
-    <Pokedex/>
+    <PokemonCard/>
   </div>;
 }
 
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,9 +1,26 @@
-function Pokedex(){
-    return <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-             alt="Pokemon de type plante, bulbasaur" /> 
-        <figcaption>bulbasaur</figcaption>
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
+function PokemonCard() {
+  const pokemon = pokemonList[0]
+  return (
+    <figure>
+      {pokemon.imgSrc === undefined ? <p>???</p> : 
+      <img 
+        src={pokemon.imgSrc}
+        alt={pokemon.name}
+      /> }
+      <figcaption>{pokemon.name}</figcaption>
     </figure>
+  );
 }
 
-export default Pokedex;
+export default PokemonCard;
